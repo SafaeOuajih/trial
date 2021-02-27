a753233591459db88a9b251d4583dc6f1d4cb986
Ouajih Safae <safae.ouajih44@gmail.com>
2021-02-28 00:45:16 +0100

    Cmake

---
+++ Cmake_tests/build/CMakeFiles/Safeik.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Safeik.dir/main.cpp.o"
+  "Safeik"
+  "Safeik.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Safeik.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Cmake_tests/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main(){
+
+  std::cout<<"\n it worked \n";
+  return 0;
+}
